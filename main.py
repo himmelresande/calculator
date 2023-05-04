7d416665e5aba593f1b8f5b95dd9451cfd5b8962
Korolev Alexander <s6korolev@mail.ru>
2023-05-04 15:09:11 +0300

    created repo

--- main.py
+++ main.py
@@ -167,6 +167,8 @@ class Calculator(QWidget):
         elif self.op == "/":
             if self.num_2 !=0:
                 self.input.setText(answer(self.num_1 / self.num_2))
+            else:
+                self.input.setText("na 0 delit' nelzya")
         self.already_used = 1
         self.num_1 = float(self.input.text())
 
